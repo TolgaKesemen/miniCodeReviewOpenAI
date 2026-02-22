7d251567a864369cd8b456f4016db4a9cc31bec2
tolga.kesemen <tolgaksmn@gmail.com>
2026-02-22 16:36:24 +0300

    SwaggerConfig

--- src/main/java/mini/codereview/config/SwaggerConfig.java
+++ src/main/java/mini/codereview/config/SwaggerConfig.java
@@ -1,24 +1,24 @@
-//package mini.codereview.config;
-//
-//import io.swagger.v3.oas.models.ExternalDocumentation;
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info()
-//                        .title("Mini Code Review API")
-//                        .version("1.0")
-//                        .description("Mini Code Review API")
-//                )
-//                .externalDocs(new ExternalDocumentation()
-//                        .url("https://github.com/TolgaKesemen/miniCodeReviewOpenAI"));
-//    }
-//
-//}
+package mini.codereview.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Mini Code Review API")
+                        .version("1.0")
+                        .description("Mini Code Review API")
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .url("https://github.com/TolgaKesemen/miniCodeReviewOpenAI"));
+    }
+
+}
